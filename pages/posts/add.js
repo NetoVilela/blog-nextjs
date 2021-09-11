6d3340c4e91c13cf061f0482a47c168b41ef676d
NetoVilela <favneto.a.v.n@gmail.com>
2021-09-10 21:51:58 -0300

    Finaliza addpostagens e responsividade

--- pages/posts/add.js
+++ pages/posts/add.js
@@ -1,25 +1,32 @@
-import { FormDiv, Form, Input, Button, TextArea, Label } from '../../src/components/Form';
+import { FormDiv, FormHead, FormBody, FormFooter, Form, Input, Button, TextArea, Label } from '../../src/components/Form';
 import { Container } from '../../src/components/Container/index';
 import SideBar from '../../src/components/SideBar';
-import {Layout} from '../../src/components/Layout';
+import { Layout } from '../../src/components/Layout';
 export default function AddPost() {
     return (
 
         <Layout>
             <SideBar />
-        
-        <Container>
-            <FormDiv>
-                <Form method='POST'>
-                    <Label for='title-input'>Insira o título</Label>
-                    <Input id='title-input' placeholder="Ex: ReactJS" />
-                    <Label for='content'>Insira o conteúdo</Label>
-                    <TextArea id='content' placeholder="Ex: ReactJS"> 
-                    </TextArea>
-                    <Button type='submit'>Cadastrar</Button>
-                </Form>
-            </FormDiv>
-        </Container>
+
+            <Container>
+                <FormDiv>
+                    <Form method='POST'>
+                        <FormHead>
+                            Cadastro de novas postagens
+                        </FormHead>
+                        <FormBody>
+                            <Label for='title-input'>Insira o título</Label>
+                            <Input id='title-input' placeholder="Ex: ReactJS" />
+                            <Label for='content'>Insira o conteúdo</Label>
+                            <TextArea id='content' placeholder="Ex: ReactJS">
+                            </TextArea>
+                        </FormBody>
+                        <FormFooter>
+                            <Button type='submit'>Criar postagem</Button>
+                        </FormFooter>
+                    </Form>
+                </FormDiv>
+            </Container>
         </Layout>
 
     )
--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -2,57 +2,147 @@ import styled from 'styled-components';
 
 export const FormDiv = styled.div`
     height: 100vh;
-    margin: 0 auto;
-    background: #fff;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-
 `;
+
+export const FormHead = styled.div`
+    background: #EDEDED;
+    width: 100%;
+    height: 50px;
+    color: #293949;
+    font-weight: bold;
+    padding: 10px;
+`;
+
+export const FormBody = styled.div`
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    flex-grow: 1;
+    padding: 1vh;
+    max-height: 280px;
+`;
+
+export const FormFooter = styled.div`
+    background: #EDEDED;
+    width: 100%;
+    height: 50px;
+    color: #293949;
+    font-weight: 400;
+    padding: 10px;
+    display: flex;
+    justify-content: flex-end;
+`;
+
 export const Form = styled.form`
     display: flex;
     flex-direction: column;
-    justify-content: center;
-    padding: 1vh;
-    border-radius: 5px;
+    background: #fff;
     width: 70%;
     height: 60vh;
+    position: relative;
+    @media (max-width: 480px) {
+        width: 100%;
+    }
+
+    @media (min-width: 481px) and (max-width: 768px) {
+        width: 100%;
+    }
+
+    @media (min-width: 769px) and (max-width: 1024px) {
+        width: 80%;
+    }
 `;
 
 export const Label = styled.label`
-    font-size: 1.3em;
-    font-weight: 400;
+    font-size: 0.8em;
+    font-weight: 500;
+    color: rgba(0,0,0,0.9);
+    margin-bottom: 10px;
+
+    @media (max-width: 480px) {
+        font-size: 1em;
+    }
+
+    @media (min-width: 481px) and (max-width: 1024px) {
+        width: 40%;
+    }
 `;
 
 export const Input = styled.input`
-    padding: 8px;
-    border: 1px solid black;
+    padding: 5px;
+    border: solid 2px rgba(237,237,237, 1);
     border-radius: 3px;
     margin-bottom: 20px;
     width: 50%;
+    :focus{
+        transition: 0.5s;
+        box-shadow: 0 0 0 0;
+        border-radius: 5px;
+        border: solid 2px rgb(51,51,51);
+        outline: 0;
+    }
+
+    @media (max-width: 480px) {
+        width: 100%;
+    }
+
+    @media (min-width: 481px) and (max-width: 768px) {
+        width: 70%;
+    }
+
+    @media (min-width: 769px) and (max-width: 1024px) {
+        width: 80%;
+    }
 `;
 
 export const TextArea = styled.textarea`
-    width: 90%;
-    max-width: 90%;
-    height: 200px;
+    width: 100%;
+    border: solid 2px rgba(224,224,224, 1);
+    height: 100px;
+    :focus{
+        transition: 0.5s;
+        box-shadow: 0 0 0 0;
+        border-radius: 5px;
+        border: solid 2px rgb(51,51,51);
+        outline: 0;
+    }
+
+    @media (max-width: 480px) {
+        width: 100%;
+    }
+
+    @media (min-width: 481px) and (max-width: 768px) {
+        width: 100%;
+    }
+
+    @media (min-width: 769px) and (max-width: 1024px) {
+        width: 80%;
+    }
 `;
 
 export const Button = styled.button`
-    color: green;
-    font-weight: 350;
-    background: #fff;
+    color: #fff;
+    font-weight: bold;
+    background: #333333;
     border-radius: 4px;
-    border: 1px solid green;
+    border: 1px solid #fff;
     cursor: pointer;
     padding: 1vh;
-    margin-top: 3vh;
-    width: 90%;
+    width: 30%;
     :hover{
         transition: 0.4s;
-        color: #fff;
-        background: green;
-        border: 1px solid #fff;
+        border: 1px solid #333333;
+    }
+
+    @media (max-width: 480px) {
+        width: 100%;
+    }
+
+    @media (min-width: 481px) and (max-width: 1024px) {
+        width: 40%;
     }
 `;
